52e030aca15d8927a8ce4ee30634c5ee0406dc9c
Ravid Dayan <dayanravid@gmail.com>
2024-05-10 04:16:50 +0300

    chnaged freelist logic and changed free data to accept nodes only

--- linkedList.c
+++ linkedList.c
@@ -1,8 +1,47 @@
 #include "node.h"
 #include "linkedList.h"
+#include "stdlib.h"
+#include "stdio.h"
 
-void newLinkedList(LinkedList *list);
+LinkedList *newLinkedList()
+{
+    LinkedList *newLinkedList = (LinkedList *)malloc(sizeof(newLinkedList));
+    if (newLinkedList == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    newLinkedList->head = NULL;
+    return newLinkedList;
+}
 
-void append(LinkedList *list, Node *newNode);
+void append(LinkedList *list, Node *newNode)
+{
+    Node *current = list->head;
+    if (current == NULL)
+    {
+        list->head = newNode;
+    }
+    else
+    {
+        while (current->next != NULL)
+        {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+}
 
-void free(LinkedList *list, void (*freeMemory)(Node, int), int type);
+void freeList(LinkedList *list, void (*freeData)(Node *))
+{
+    Node *next;
+    Node *current = list->head;
+    while (current != NULL)
+    {
+        next = current->next;
+        freeData(current);
+        free(current);
+        current = next;
+    }
+    free(list);
+}
--- linkedList.h
+++ linkedList.h
@@ -1,40 +1,15 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
 #include "node.h"
 
-typedef struct
+typedef struct LinkedList
 {
     Node *head;
 } LinkedList;
 
-void newLinkedList(LinkedList *list)
-{
-    list->head = NULL;
-}
+LinkedList *newLinkedList();
 
-void append(LinkedList *list, Node *newNode)
-{
-    Node *current = list->head;
-    if (current == NULL)
-    {
-        list->head = newNode;
-    }
-    else
-    {
-        while (current->next != NULL)
-        {
-            current = current->next;
-        }
-        current->next = newNode;
-    }
-}
+void append(LinkedList *list, Node *newNode);
 
-void free(LinkedList *list, void (*freeMemory)(Node, int), int type)
-{
-    Node *current = list->head;
-    while (current != NULL)
-    {
-        Node *temp = current;
-        current = current->next;
-        freeMemory(temp, type);
-    }
-    list->head = NULL;
-}
+void freeList(LinkedList *list, void (*freeData)(Node *));
+#endif
