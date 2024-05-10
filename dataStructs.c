fe011e03fcf7427050548a3b45424a18f5aa73cf
Ravid Dayan <dayanravid@gmail.com>
2024-05-10 19:55:48 +0300

    added node, linked list and memory structs and functions

--- dataStructs.c
+++ dataStructs.c
@@ -3,6 +3,7 @@
 #include "string.h"
 #include "stdio.h"
 
+/*@@@ Macro functions @@@*/
 Mcro *newMcro(char *name)
 {
     Mcro *newMcro = (Mcro *)malloc(sizeof(Mcro));
@@ -19,4 +20,94 @@ Mcro *newMcro(char *name)
     strcpy(newMcro->name, name);
     newMcro->data = NULL;
     return newMcro;
+}
+Mcro *mcroExists(char *newMcro, LinkedList *mcros)
+{
+    char *mcroName;
+    Mcro *mcro;
+    Node *mcroNode = mcros->head;
+    while (mcroNode != NULL)
+    {
+        mcro = (Mcro *)mcroNode->data;
+        mcroName = mcro->name;
+        if (strcmp(newMcro, mcroName) == 0)
+        {
+            {
+                return mcro;
+            }
+        }
+        mcroNode = mcroNode->next;
+    }
+    return NULL;
+}
+
+/*@@@ Node functions @@@*/
+Node *newNode(void *data)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (newNode == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+Node *getNext(Node *node)
+{
+    return node->next;
+}
+void *getData(Node *node)
+{
+    return node->data;
+}
+
+/*@@@ Linked list functions @@@*/
+LinkedList *newLinkedList()
+{
+    LinkedList *newLinkedList = (LinkedList *)malloc(sizeof(newLinkedList));
+    if (newLinkedList == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    newLinkedList->head = NULL;
+    return newLinkedList;
+}
+void append(LinkedList *list, Node *newNode)
+{
+    Node *current = list->head;
+    if (current == NULL)
+    {
+        list->head = newNode;
+    }
+    else
+    {
+        while (current->next != NULL)
+        {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+}
+
+/*@@@ Memory functions @@@*/
+void setBits1to2(Memory *data, int number) {
+    data->bits |= (number & 0b11);
+}
+void setBits3to4(Memory *data, int number) {
+    data->bits |= ((number & 0b11) << 2);
+}
+void setBits5to6(Memory *data, int number) {
+    data->bits |= ((number & 0b11) << 4);
+}
+void setBits7to10(Memory *data, int number) {
+    data->bits |= ((number & 0b1111) << 6);
+}
+void setSignedIntBits3to14(Memory *data, int number) {
+    data->bits |= ((number & 0b1111111111111) << 2);
+}
+void setCharBits3to14(Memory *data, char letter) {
+    data->bits |= ((letter & 0b1111111111111) << 2);
 }
--- dataStructs.h
+++ dataStructs.h
@@ -9,25 +9,48 @@
 #define MEMORY 2
 #define LABEL 3
 #define TOKEN 4
+#define MEMORY_SIZE 4096
+
+
+typedef struct Label
+{
+    char *name;
+    int property;
+    int value;
+} label;
 
 typedef struct Mcro
 {
     char *name;
     char *data;
 } Mcro;
-
-typedef struct label
-{
-    char *name;
-    int property;
-    int address;
-} label;
-typedef struct token
-{
-    char *token;
-} token;
-
 Mcro *newMcro(char *name);
+Mcro *mcroExists(char *newMcro, LinkedList *mcros);
 
+typedef struct LinkedList
+{
+    Node *head;
+} LinkedList;
+LinkedList *newLinkedList();
+void append(LinkedList *list, Node *newNode);
 
+typedef struct Node
+{
+    void *data;
+    struct Node *next;
+} Node;
+Node *newNode(void *data);
+Node *getNext(Node *node);
+void *getData(Node *node);
+
+typedef struct Memory
+{
+    unsigned short bits : 14; 
+} Memory;
+void setBits1to2(Memory *data, int number);
+void setBits3to4(Memory *data, int number);
+void setBits5to6(Memory *data, int number);
+void setBits7to10(Memory *data, int number);
+void setSignedIntBits3to14(Memory *data, int number);
+void setCharBits3to14(Memory *data, char letter);
 #endif
